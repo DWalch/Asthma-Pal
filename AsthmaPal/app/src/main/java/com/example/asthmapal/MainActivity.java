dd01fc88bcd840763f1505350941c93a79f6ece3
DWalch <DWalch@users.noreply.github.com>
2020-11-05 14:34:21 -0800

    add initial project explanation

--- AsthmaPal/app/src/main/java/com/example/asthmapal/MainActivity.java
+++ AsthmaPal/app/src/main/java/com/example/asthmapal/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.asthmapal;
-
+/*
+COMP3540 Human Computer Interaction
+Asthma Pal
+Doug Walch T00536730
+Date: November 5th 2020
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
