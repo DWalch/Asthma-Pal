ce1bf098fe54bae63bf46363a9fead98f9cdcbd1
DWalch <DWalch@users.noreply.github.com>
2020-11-07 14:31:42 -0800

    swap activities
    working on getting buttons to swap to the different activities

--- AsthmaPal/app/src/main/java/com/example/asthmapal/MainActivity.java
+++ AsthmaPal/app/src/main/java/com/example/asthmapal/MainActivity.java
@@ -7,13 +7,35 @@ Date: November 5th 2020
  */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.asthmapal.ui.login.LoginScreen;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtLaunchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtLaunchActivity = (Button) findViewById(R.id.nextButton1);
+
+        mBtLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+    }
+
+    private void launchActivity() {
+                Intent intent = new Intent(this, LoginScreen.class);
+                startActivity(intent);
+            }
+        }
     }
 }
