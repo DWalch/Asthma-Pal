cfb1fba11e4da821fd9193e81222b30d43eaf733
Alex <computerfrisnmail@gmail.com>
2020-11-09 16:31:46 -0800

    Finished login. Works well and we don't need to worry about full validation of it yet however it is required if we want to rest passwords

---
+++ app/src/main/java/com/example/Asthma_Pal/DailyEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.Asthma_Pal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_entry);
+    }
+}
