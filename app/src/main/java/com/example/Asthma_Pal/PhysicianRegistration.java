1369f416dc85bb1581045cc965736a5e4f014e53
DWalch <DWalch@users.noreply.github.com>
2020-11-15 14:15:19 -0800

    physician registration page

---
+++ app/src/main/java/com/example/Asthma_Pal/PhysicianRegistration.java
@@ -0,0 +1,14 @@
+package com.example.Asthma_Pal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhysicianRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physician_registration);
+    }
+}
